e762df33ba30b1a33d1f8d7e1fb3c9494892688a
Victorkhuraijam <khuraijamvictor@gmail.com>
2025-03-01 10:51:34 +0530

    deploying on render

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,15 +5,11 @@ import cookieParser from 'cookie-parser'
 
 const app = express()
 
-const allowedOrigins = [
-  'http://127.0.0.1:5173',
-  'http://127.0.0.1:5174',
-  'http://localhost:5173',
-  'http://localhost:5174'
-];
+const allowedOrigins = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : [];
+
 app.use(cors({
   origin: (origin, callback) => {
-    console.log('Request Origin:', origin); // Debugging
+    // console.log('Request Origin:', origin); // Debugging
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
