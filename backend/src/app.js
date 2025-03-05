2d90bef8dcff1f997de4784223e1760ecab0e60e
Victorkhuraijam <khuraijamvictor@gmail.com>
2025-03-05 10:50:35 +0530

    cors settings

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,8 +30,8 @@ app.use(cors({
     callback(new Error(`Not allowed by CORS: ${origin} is not allowed`));
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  // allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 
